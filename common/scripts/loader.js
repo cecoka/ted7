047f2ca30cf416e58507f1339b9b777abe444a2e
cecoka <39671880+cecoka@users.noreply.github.com>
2018-07-18 13:15:34 +0100

    Update loader.js

--- common/scripts/loader.js
+++ common/scripts/loader.js
@@ -1,16 +1,18 @@
 window.onload = function() {
     var loadingScreen = document.getElementById("loadingScreen");
     var loader = document.getElementById("loader");
-    var loaderText = document.getElementById("loaderText");     
-    loaderText.parentNode.removeChild(loaderText);
+    var loaderLogo = document.getElementById("loaderLogo");  
+    var body = document.getElementsByTagName("body")[0];  
     loader.style.display = "none";
+    loaderLogo.style.display = "none";
+    body.style.overflow = "visible";
     if (document.readyState === "complete"){
         var loaderScreenWidth = setInterval(changeLoaderScreenWidth, 10);
         var slider = 100;
         function changeLoaderScreenWidth(){
-            if (slider > 0){
+            if (slider > 0 && screen.width >= 1024){
                 slider --;
-//                console.log(slider+"%"); //use for debugging
+//               console.log(slider+"%"); //use for debugging
                 loadingScreen.style.width = slider+"%";
             }
             else {
